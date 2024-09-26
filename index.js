19d8edb34c20792d1b229e10d9710c22e6f3ef3b
mayconfer123 <fernandesmaycon813@gmail.com>
2024-09-26 20:27:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,319 @@
+//alert(numero1 =numero2) 
+
+//var numero1 = Number(prompt("qual o primeiro numero"))
+//var numero2 = Number( prompt("qual o segundo numero"))
+
+//alert("o resultado da soma dos dois é:" +(numero1 + numero2))
+//alert("o resultado da subtracao dos dois é:" +(numero1 - numero2))
+//alert("o resultado da divisao dos dois é:" +(numero1 / numero2))
+//alert("o resultado da multiplicacao dos dois é:" +(numero1 * numero2))
+
+
+//var nota1 = Number(prompt("qual o primeiro numero"))
+//var nota2 = Number( prompt("qual o segundo numero"))
+/*
+var nome = prompt("qual o nome do aluno:")
+var nota1 =Number (prompt("qual a primeira nota do "+(nome)))
+var nota2 =Number (prompt("ok. Primeira nota "+nota1+ " Qual a segunda nota:"))
+alert("a media final será:" + (nota1+nota2)/2)      
+*/
+
+
+//var numero1
+//var numero2
+//var numero3
+
+//alert("numero2 + numero2")
+
+
+//if ((nota1+nota2)/2  >= 6){
+//lert("aluno APROVADO")
+//}
+//lse{ 
+//alert("aluno REPRVADO")
+//}
+ // Var idade = number (prompt("qual a sua idade?"))
+  //if (idade >= 18){
+  //alert("maior de idade")
+ // }
+ // else{
+ //   alert("menor de idade")
+ // } 
+
+/*
+ tofixed formata um numero
+
+ var peso=number(prompt("qual o seu peso?"))
+ var autara=number(prompt("qual a sua autura?"))
+ var imc =(peso/(autura*autura)).toFixed(2)
+ if(imc<18.5){
+}
+ else if(imc >=25)&(imc<30)){
+ alert ("pwso normal seu imc e "+imc)
+}
+ elseif((imc >=25)&(imc<30)){
+ alert("voce esta no sobrepeso seu imc e de "+imc)
+}
+ else if((imc >=35)&(imc<40)){
+ alert ("voce estar na obasidade nivel 1 seu imc e de "+imc)
+ }
+ else if((imc >=35)&(imc<40)){
+ alert("vose esta na obesidade tipo e ele e de "+imc)
+ }
+ else if(imc >=40)){
+ alert("voce esta no nivel mrobido e seu imc e de "+imc+" va para uma academia, gigante")
+ }
+ */
+
+
+
+
+ //var listaDeCursos = ["progracao ", "enfermagem", "estetica", "farmacia", "seguranca", "admistraçao", " informatica" ]
+ /*
+  
+ alert( listadecursos[1] ) 
+ alert( listadecursos[0] )
+ alert( listadecursos[2] )
+ alert( listadecursos[3] )
+ alert( listadecursos[4] ) 
+ alert( listadecursos[5] ) 
+ alert( listadecursos[6] ) 
+ alert( listadecursos[7] )
+*/
+//listaDeCursos.forEach(curso => alert(curso))
+/*
+//for(var i=0; i<=6; i++){
+  alert(listaDeCursos[i])
+}
+*/
+
+ /*
+ var nota = prompt("quantas notas seraoavaliadas?")
+ for(var i=i;<=nota={;i++){
+  prompt("qual o valor da nota"+i)
+ }
+ var media = [nota / i++] 
+ alert("a media é " media.toFixed(2))
+*/
+ 
+/*
+//prompt inicial para coletar quantas notas aerao utilizada no calculo
+ var qtdeNotas = prompt("quantas notas serao avaliadas")
+
+ //estrutura de repeticao que irá rodar uma vez para cada nota 
+ var acumuladorNotas = 0;
+
+
+ for (var i = 1; i<= qtdeNotas; i++){
+  //pront para perguntar a nota do aluno atual
+  var nota = prompt("digito a nota" + i)
+  //variavel receber o valor dela mensa mais a nota do aluno atual
+  acumuladorNotas = acumuladorNotas + Number(nota)
+ }
+//criar um alerta com a divisao do valor acumulado pela qtde de notas
+ alert("a média é:" + (acumuladorNotas /Number(qtdeNotas)))
+*/
+
+/*
+ var listadeNomes = ["Maylon", "Arthur", "Ryan", "Raphaael", "Maria", "Gabriel", "Macoly", "Dalton", "Patrick" ]
+ var indiceProcurado = prompt("qual o indice do aluno?")
+ alert(listaDeNomes[indiceProcurado])
+*/
+ 
+//var n =Number(prompt("digite um numero:"))
+//for(i=0;i<=;i++){
+  //alert(${n} *${i} = $(n))
+//}
+//alert()
+
+//var listaDEJoghos = ["fifa", "call of duty", "red dead", "cs"]
+
+//var novoJogo = prompt("digite um jogo para adicionar")
+//push serve para ADICIONAR um JOGO REGISTRO em uma LISTA
+//listaDeJogos.push( novoJogo )
+//listaDEJogos.pop()  //REMOVE O ULTIMO ELEMENTO DE LISTA
+//listaDeJogos.shift()  //REMOVE O PRIMEIRO DA LISTA
+//listaDeJogos.splice(2,1)  //REMOVE 1 ELEMENTO A PARTI DO INDECE?
+//listaDeJogos.reverse()  //INVERTE A ORDEM
+
+ //var indice = listaDeJogos.indexOf("fifa")
+
+ //alert( indece )
+////alert( lista de jogos[4] )
+//listaDeJogos.forEach(jogo => alert("estou jogando" + jogo))
+
+/*
+var listaDeJogos = ["fifa", "call of duty", "red dead", "cs"]
+
+var qtdeJogosNovos = prompt("quantos jogos voce quer adicionar?")
+
+for(var i=0; i<qtdeJogosNovos; i++){
+  var jogo = prompt("digite o nome do jogo")
+  listaDeJogos.push(jogo)
+}
+
+alert(listaDeJogos)
+
+*/
+
+/*
+var operacao = pronpt("1- saldo\n2- transferencia\n3- sair do sistema")
+ var i = 0;
+ while(i < 10){
+  alert(1)
+  i++   
+ }
+
+ 
+ while (operacao !=3){
+ var operacao = pronpt("1- saldo\n2- transferencia\n3- sair do sistema")
+}
+*/
+
+//propriedade do objeto curso
+//nome do curso
+//aluno
+//cargohorario
+
+
+/*
+var listadeNomes = ["Maylon", "Arthur", "Ryan", "Raphaael", "Maria", "Gabriel", "Macoly", "Dalton", "Patrick" ]
+
+var curso = {
+  nome: "Logica de programaçao"
+  alunos: listadenomes
+  cargahoraria: "60hrs",
+  materia: "Javascript"
+  datainicio: "27/08/2024",
+  datafinal: "29/09/2024"
+}
+
+alert(curso.nome + "é um curso com"
+  curso. cargahoraria.h + "h de carga horario")
+  */
+
+/*
+  function rotinadiaria() {
+    alert("acorda")
+    alert("arruma acama")
+    alert("ir trabalhar")
+    alert("voltar para casa")
+    alert("dormir")
+  
+
+  rotinadiaria()
+
+  rotinadiaria()
+
+  rotinadiaria()
+
+  rotinadiaria()
+
+  rotinadiaria()
+*/
+
+/*
+  function somarNumeros(num1, num2){
+    var num1 = Number(pronpt("digite o primeiro numero"))
+    var num2 = Number(Number("digite o segundonumero"))
+    var resultado = num1 +num2
+    alert(resultado)
+}
+ somarNumeros()
+*/
+
+/*
+function saudacao(mome) {
+alert("Seja bem vindo," + nome)
+alert("digite a operacao: \n1- tal \n2- Tal")
+}
+
+saudacao("Daniel")
+*/
+
+
+/*
+{
+//function multiplicacao ()
+var num1 = Number(prompt("digite o primeiro numero "))
+var num2 = Number(prompt("digite o segundo numero"))
+var num3 = Number(prompt("digite o terceiro numero"))
+var resultado = num1 * num2 * num3
+alert(resultado)
+}
+
+multiplicacao()
+*/
+
+/*
+function classificador(){
+var numero = prompt("qual o número?")
+if(numero>10){
+  alert(mumero + " maior do que dez")
+}  
+else if (numero==10){
+  alert(numero + " é igual a dez")
+}
+else{
+  alert(numero + "menor que dez")
+}
+}
+
+classificador()
+*/
+
+//var cep = "38702058"
+//var cepFormatado = cep.substring(0,5) + "-" + cep,substring(5, 8)
+
+//alert( cepFormatador)
+
+/*
+var cpf = "13324509123"
+alert(cpf.substring(0,3)+ "." +  cpf.substring(3,6) + "." + cpf.substring(6,9) + "-" + cpf.substring(9,11) )
+*/
+
+/*
+alert("Ola, seja bem vindo ao sistema")
+var operaçao = prompt("o que voce deseja fazer? \n1- ver jogos disponiveis \n2- cadastrar jogo")
+if(operacao ==1){
+  alert("jogos disponiveis")
+}
+else if(operacao ==2){
+
+}
+*/
+
+{
+var nome = prompt("qual seu nome?")
+
+alert("Ola, seja bem vindo ao NUMBANK " + nome)
+var I=0
+var saldo = 0
+while(I==){
+  var operacao=prompt("o que voce deseja fazer?\n1-ver extrato disponivel \n2-retira o dinheiro  \n3-depositar o dinheiro \n4-pagar conta!")
+
+  if(operacao==1){
+    if(saldo<=0){alert("BANCO ESTA FORA DE AREA, procure a agencia mais proximo ou ligue dd 290 ")}
+    else{
+}
+  }
+
+  else if(operacao==2){
+    var retirada = prompt("quanto seria o valor para retirada?")
+    alert("voce nao tem saldo em sua conta,deposite uma valor poder sacalo!!")
+  
+  }
+   else{
+    saldo = saldo - retirada
+    alert(saldo)
+   }
+
+}
+
+  else if(operacao== 3){
+    var depositar = Number(prompt("quanto voce deseja depositar em sua conta?"))
+    saldo = saldo + depositar
+    alert("voce tem R$ "+saldo+" de saldo")
+  }
+
+  }                          
